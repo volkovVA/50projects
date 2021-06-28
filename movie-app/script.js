56e76bc99e9b4e72b06f96153c5dc9512e1106b0
volkovVA <volkov.slava@hotmail.com>
2021-06-28 16:02:39 +0300

    feat: add showMovies function

--- movie-app/script.js
+++ movie-app/script.js
@@ -14,6 +14,39 @@ async function getMovies(url) {
     showMovies(data.results);
 }
 
+function showMovies(movies) {
+    movies.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        `
+
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
