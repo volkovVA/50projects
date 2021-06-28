dc32b8e1c3d9580016ce37259e089cddcb010242
volkovVA <volkov.slava@hotmail.com>
2021-06-28 16:01:57 +0300

    feat: add fetch and search methods

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,3 +1,31 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3d7312866b21986c2925e17836fcffe4&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3d7312866b21986c2925e17836fcffe4&query="'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
+        main.innerHTML = ''
+
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
