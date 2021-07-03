d82f72c21cf433263f98015b204fe899e5336a0e
volkovVA <volkov.slava@hotmail.com>
2021-07-03 19:17:29 +0300

    feat: add get user and search functions

---
+++ github-profiles/script.js
@@ -0,0 +1,25 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+
+        console.log(data)
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const user = search.value
+
+    if (user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
