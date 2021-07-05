a789a6f016832ae779bd0d11a002c5a5f30bf180
volkovVA <volkov.slava@hotmail.com>
2021-07-05 20:46:32 +0300

    feat: add create card function

--- pokedex/script.js
+++ pokedex/script.js
@@ -17,6 +17,8 @@ const colors = {
     normal: '#F5F5F5'
 }
 
+const main_types = Object.keys(colors)
+
 const fetchPokemons = async () => {
     for(let i = 1; i < pokemon_count; i++) {
         await getPokemon(i)
@@ -27,7 +29,34 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data);
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor = color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span> </small>
+    </div>
+    `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+    poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemons()
