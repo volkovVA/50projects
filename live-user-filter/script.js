84a4aeb3c8324b34180bdd28b3ca3ab6a0bd35c4
volkovVA <volkov.slava@hotmail.com>
2021-07-08 16:42:12 +0300

    feat: add filter function

--- live-user-filter/script.js
+++ live-user-filter/script.js
@@ -4,6 +4,8 @@ const listItems = []
 
 getData()
 
+filter.addEventListener('input', e => filterData(e.target.value))
+
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50')
 
@@ -25,5 +27,16 @@ async function getData() {
         `
 
         result.appendChild(li)
+    })
+}
+
+function filterData(searchTerm) {
+    listItems.forEach(item => {
+        console.log(item)
+        if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
     })
 }
